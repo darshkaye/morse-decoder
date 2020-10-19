033cea535c2b992d1b8a9acac884df015ddcba65
Darya Yermakova <yermakova.dasha@gmail.com>
2020-10-20 00:58:21 +0300

    feat: develop morse decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.toString().match(/.{1,10}/g);
+    let result = "";
+
+    expr.forEach(symbolCode => {
+      if (symbolCode === "**********") {
+        result = result + " ";
+      } else {
+        let charCode = "",
+            chunkedCode = symbolCode.toString().match(/.{1,2}/g);
+        for (let i = 0; i < chunkedCode.length; i++) {
+          if (chunkedCode[i] === '00') {
+            continue;
+          } else if (chunkedCode[i] === '10') {
+            charCode = charCode + ".";
+          } else if (chunkedCode[i] === '11') {
+            charCode = charCode + "-";
+          }
+        }
+        result = result + MORSE_TABLE[charCode];
+      }
+    });
+
+    return result;
 }
 
 module.exports = {
